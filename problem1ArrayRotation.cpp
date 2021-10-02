2d781271a1aac962e45bd5769067f5a5393ec8e8
Ayush Nandanwar <80102992+ayushnandanwar08@users.noreply.github.com>
2021-10-02 15:31:31 +0530

    Add files via upload

---
+++ problem1ArrayRotation.cpp
@@ -0,0 +1,24 @@
+/*
+    Array Rotation- Hackkerank contest solution Team Heavy Coder 8th sept 2021
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >>n;
+    int a[n];
+    for(int i = 0;i < n;i++){
+        cin >>a[i];
+    }
+    int temp = a[0];
+    for(int i = 0;i < n-1;i++){
+        a[i] = a[i+1];  // assigning to left 
+    }
+    a[n-1] = temp;
+    for(int i = 0;i < n;i++){
+        cout <<a[i]<<" ";
+    }
+
+}
